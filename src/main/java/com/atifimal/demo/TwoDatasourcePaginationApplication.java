9e8c1ec1ee58b48f95d25ace5c985240c912ba01
Atif Imal <mehmetatif@mail.ru>
2024-02-16 02:35:12 +0300

    Spring Initializr

---
+++ src/main/java/com/atifimal/demo/TwoDatasourcePaginationApplication.java
@@ -0,0 +1,13 @@
+package com.atifimal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoDatasourcePaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoDatasourcePaginationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atifimal/demo/TwoDatasourcePaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atifimal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoDatasourcePaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
